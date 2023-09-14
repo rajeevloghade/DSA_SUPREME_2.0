81bf44a520fdefd0dddab81b16e973a9136d54ee
rajeevloghade <rajeevloghade010@gmail.com>
2023-09-14 22:40:02 +0530

    Searching: Pivot element in the Array

---
+++ Searching Algorithm/PivotElementInArray.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int pivotElemetInArray(vector<int> array)
+{
+    int start = 0;
+    int end = array.size();
+
+    while (start < end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (array[mid] >= array[0])
+        {
+            // we are at first line on graph
+            start = mid + 1;
+        }
+        else
+        {
+            // we are at second line on graph
+            end = mid;
+        }
+    }
+    return start; // we can return start(index) or end(index)
+}
+
+int main()
+{
+
+    vector<int> array = {7, 9, 1, 2, 3};
+
+    cout << "Pivot Element in array at index : " << pivotElemetInArray(array)
+         << endl;
+
+    return 0;
+}
+
+/**
+Pivot element in the Array : draw array element in graph format
+
+Time Complexity :  O(log n)
+Space Complexity : O(1)
+
+*/
