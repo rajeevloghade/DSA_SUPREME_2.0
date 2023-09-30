a2434a7933fecfef8fbc5592a11332068517cc0c
rajeevloghade <rajeevloghade010@gmail.com>
2023-09-30 15:32:34 +0530

    String : Remove All Adjacent Duplicates

---
+++ Char Arrays & Strings/RemoveAllAdjacentDuplicates.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+string removeAllAdjacentDuplicates(string input)
+{
+    string output = "";
+
+    for (int index = 0; index < input.length(); index++)
+    {
+        // insert into temp string if temp string's right most element doesn't match
+        // with input string current element
+        if (output.length() >= 0 && input[index] != output[output.length() - 1])
+        {
+            output.push_back(input[index]);
+        }
+        else
+        {
+            // delete if matches
+            output.pop_back();
+        }
+    }
+
+    return output;
+}
+int main()
+{
+    string input;
+    cout << "Please enter your input string: ";
+    cin >> input;
+    cout << "After removing all adjacent duplicate : "
+         << removeAllAdjacentDuplicates(input) << endl;
+}
